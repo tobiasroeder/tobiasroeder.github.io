acc2a2be318b5045c7f7a9faa1d10caad2df1b9a
Tobias Röder <tr@7iu.de>
2019-04-11 23:57:35 +0200

    1.1.0

--- imagebox/addimagebox.js
+++ imagebox/addimagebox.js
@@ -1,29 +1,38 @@
 // ======================================================
-// ImageBox v1.0.4
-// addImageBox v1.0.1
+// ImageBox v1.1.0
+// addImageBox v1.0.2
 //
 // Creative Commons Attribution 4.0 International License
 // https://tobiasroeder.github.io/imagebox/license
 //
 // https://github.com/tobiasroeder/imagebox
-// Copyright 2019 ImageBox
+// Copyright 2018 ImageBox
 //
 // ======================================================
 
 // this file embed always the newest version from the imagebox, the css and the js file
 window.onload = function() {
-    var ibVersion = "1.0.4";
-    var filePath = "https://tobiasroeder.github.io/imagebox/"+ibVersion+"/imagebox.min.";
+	
+	var ibVersion,
+		addimageboxVersion = document.querySelectorAll('script[data-addimagebox-version]')[0].getAttribute('data-addimagebox-version');
+	
+	if (addimageboxVersion) {
+		ibVersion = addimageboxVersion;
+	} else {
+		ibVersion = '1.1.0';
+	}
+	
+	var filePath = 'https://tobiasroeder.github.io/imagebox/' + ibVersion + '/imagebox.min.';
 
     // create link tag for the css file
-	var cssLink = document.createElement("link");
-	cssLink.setAttribute("rel", "stylesheet")
-	cssLink.setAttribute("type", "text/css");
-	cssLink.setAttribute("href", filePath+"css");
+	var cssLink = document.createElement('link');
+	cssLink.setAttribute('rel', 'stylesheet')
+	cssLink.setAttribute('type', 'text/css');
+	cssLink.setAttribute('href', filePath + 'css');
 	document.head.appendChild(cssLink);
 
-    // create link tag for the script file
-	var jsScript = document.createElement("script");
-	jsScript.setAttribute("src", filePath+"js");
+    // create link tag for the css file
+	var jsScript = document.createElement('script');
+	jsScript.setAttribute('src', filePath + 'js');
 	document.body.appendChild(jsScript);
 }
