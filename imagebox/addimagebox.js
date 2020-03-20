9830f57ce1cfc79a15a8b2c8f664bbec7dfe74f1
Tobias Röder <tr@7iu.de>
2020-03-20 16:18:32 +0100

    addImageBox 1.1.0

--- imagebox/addimagebox.js
+++ imagebox/addimagebox.js
@@ -1,38 +1,38 @@
 // ======================================================
-// ImageBox v1.1.0
-// addImageBox v1.0.2
+// addImageBox v1.1.0
 //
 // Creative Commons Attribution 4.0 International License
 // https://tobiasroeder.github.io/imagebox/license
 //
 // https://github.com/tobiasroeder/imagebox
-// Copyright 2018 ImageBox
+// Copyright 2020 ImageBox
 //
 // ======================================================
 
 // this file embed always the newest version from the imagebox, the css and the js file
 window.onload = function() {
-	
+
+    // set variables
 	var ibVersion,
-		addimageboxVersion = document.querySelectorAll('script[data-addimagebox-version]')[0];
-	
-	if (addimageboxVersion) {
-		ibVersion = addimageboxVersion.getAttribute('data-addimagebox-version');
-	} else {
-		ibVersion = '1.1.0';
-	}
-	
-	var filePath = 'https://tobiasroeder.github.io/imagebox/' + ibVersion + '/imagebox.min.';
+		addimagebox = document.querySelector('script[data-addimagebox]'),
+		addimageboxVersion = document.querySelector('script[data-addimagebox-version]'),
+        ie;
+
+    // define version
+    ibVersion = (addimageboxVersion ? addimageboxVersion.dataset.addimageboxVersion : '1');
+
+    // get ie
+    ie = (addimagebox ? '.' + addimagebox.dataset.addimagebox : '');
 
     // create link tag for the css file
 	var cssLink = document.createElement('link');
 	cssLink.setAttribute('rel', 'stylesheet')
 	cssLink.setAttribute('type', 'text/css');
-	cssLink.setAttribute('href', filePath + 'css');
+	cssLink.setAttribute('href', 'https://cdn.jsdelivr.net/gh/tobiasroeder/imagebox@' + ibVersion + '/dist/imagebox.min.css');
 	document.head.appendChild(cssLink);
 
-    // create link tag for the css file
+    // create script tag for the js file
 	var jsScript = document.createElement('script');
-	jsScript.setAttribute('src', filePath + 'js');
+	jsScript.setAttribute('src', 'https://cdn.jsdelivr.net/gh/tobiasroeder/imagebox@' + ibVersion + '/dist/imagebox' + ie + '.min.js');
 	document.body.appendChild(jsScript);
 }
